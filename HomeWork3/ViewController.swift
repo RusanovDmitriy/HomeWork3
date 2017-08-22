df46f96958016deb305bf3895e308cb2cf887f91
Dima Rusanov <roarisdour@gmail.com>
2017-08-22 22:50:06 +0300

    Самостоятельная работа

--- HomeWork3/ViewController.swift
+++ HomeWork3/ViewController.swift
@@ -13,10 +13,39 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
+        let day = 34
+        print(whatADay(day: day))
 
     }
-    
+    // Блок 0 Самостоятельная работа
+    /* Написать метод, в который передается номер дня (1-7) и в консоль выводится день недели (Понед - Воскр)
+    Примечание: кто сделал на уроке, может не делать
+    Усовершенствовать предыдущий метод так, что передав 8 выведется снова же Понедельник, а передав 9 выведется Вторник, 14 - воскресенье, 16 - вторник и тд для любых чисел.
+    Примечание: кто сделал на уроке, может не делать
+    */
+    func whatADay(day: Int) -> String {
+        let newDay = day / 7
+        switch newDay {
+        case 1:
+            return "Понедельник"
+        case 2:
+            return "Вторник"
+        case 3:
+            return "Среда"
+        case 4:
+            return "Четверг"
+        case 5:
+            return "Пятница"
+        case 6:
+            return "Суббота"
+        case 7:
+            return "Воскресенье"
+        default:
+            return "You failed"
+        }
+    }
+
+
 
 }
 
